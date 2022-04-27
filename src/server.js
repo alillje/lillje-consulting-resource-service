1a3e133bfb78bc35ebefeb6ca8c0cb88b0eff59e
Andreas Lillje <al225wg@student.lnu.se>
2022-04-27 13:43:55 +0200

    Edit cors origin policy as env var

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ try {
   const app = express()
 
   app.use(cors({
-    origin: ['http://localhost:3000', 'https://lillje-consulting-customer.herokuapp.com/'],
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
     optionSuccessStatus: 200
   }))
@@ -28,6 +28,7 @@ try {
 
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
